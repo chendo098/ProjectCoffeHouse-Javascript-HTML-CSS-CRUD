9ae18ee1ab50c1ccfd5b6a10c0bba028b4c46b99
maurogrezzana <76817554+maurogrezzana@users.noreply.github.com>
2021-04-11 15:34:35 -0300

    Estilo del Admin y modificacion de ID login.js

--- admin.js
+++ admin.js
@@ -89,11 +89,17 @@ function mostrarDetalle(id) {
   const productoEncontrado = productos.find((producto) => producto.id === id);
   const detalleDiv = document.getElementById("detalleProducto");
   const detallesProducto = `
+      <div class="row">
+      <div class="col">
       <p>Tipo: ${productoEncontrado.tipo}</p>
       <p>Marca: ${productoEncontrado.marca}</p>
       <p>Descripción: ${productoEncontrado.descripcion}</p>
       <p>Precio: ${productoEncontrado.precio}</p>
       <p>Stock: ${productoEncontrado.stock}</p>
+      </div>
+      <div class="col" style="margin-top: 25px; margin-bottom: 25px;"><img src="${productoEncontrado.imagen}" alt="..." style="width: 250px; height: 125px; margin-top: 25px; margin-bottom: 25px;"></div>
+      </div>
+      
   `;
   detalleDiv.innerHTML = detallesProducto;
 }
@@ -173,22 +179,21 @@ const limpiarFiltro = () => {
   alerta.classList.add("d-none");
 };
 
-
 /// TABLA LOGIN
 
-const editarFormUsuarios = document.getElementById('formularioEditarUsuarios');
-const nombreEditar = document.getElementById('editarNombre');
-const apellidoEditar = document.getElementById('editarApellido');
-const passEditar = document.getElementById('editarPass');
-const emailEditar = document.getElementById('editarEmail');
-const usuariosTabla = document.getElementById('tablaUsuarios');
-const json2 = localStorage.getItem('usuarios'); // Traer de localStorage el dato asociado a la key "usuarios".
+const editarFormUsuarios = document.getElementById("formularioEditarUsuarios");
+const nombreEditar = document.getElementById("editarNombre");
+const apellidoEditar = document.getElementById("editarApellido");
+const passEditar = document.getElementById("editarPass");
+const emailEditar = document.getElementById("editarEmail");
+const usuariosTabla = document.getElementById("tablaUsuarios");
+const json2 = localStorage.getItem("usuarios"); // Traer de localStorage el dato asociado a la key "usuarios".
 let usuarios = JSON.parse(json2) || []; // Convertir datos de un string JSON a código JavaScript.
-let usuarioId = '';
+let usuarioId = "";
 
 function mostrarUsuarios() {
-  const usuariosMap = usuarios.map(function (usuario) { 
-      return `
+  const usuariosMap = usuarios.map(function (usuario) {
+    return `
           <tr>
              <td>${usuario.correo}</td>
              <td>${usuario.nombre}</td>
@@ -204,8 +209,8 @@ function mostrarUsuarios() {
              </td>
           </tr> 
           `;
-  })
-  usuariosTabla.innerHTML = usuariosMap.join('')
+  });
+  usuariosTabla.innerHTML = usuariosMap.join("");
 }
 
 function eliminarUsuario(id) {
@@ -213,17 +218,17 @@ function eliminarUsuario(id) {
   if (!confirmar) {
     return;
   }
-  const usuariosFiltrado = usuarios.filter((usuario) => usuario.id !== id)
+  const usuariosFiltrado = usuarios.filter((usuario) => usuario.id !== id);
   const json2 = JSON.stringify(usuariosFiltrado);
-  localStorage.setItem('usuarios', json2);
+  localStorage.setItem("usuarios", json2);
   usuarios = usuariosFiltrado;
   mostrarUsuarios();
 }
 
 function mostrarDetallesUsuarios(id) {
   const usuarioEncontrado = usuarios.find((usuario) => usuario.id === id);
-  const detalleDiv = document.getElementById('detalleUsuario');
-  const fechaUsuario = new Date (usuarioEncontrado.registro);
+  const detalleDiv = document.getElementById("detalleUsuario");
+  const fechaUsuario = new Date(usuarioEncontrado.registro);
   const detallesUsuarios = `
       <p>Nombre: ${usuarioEncontrado.nombre}</p>
       <p>Apellido: ${usuarioEncontrado.apellido}</p>
@@ -242,27 +247,27 @@ function cargarModalEditarUsuarios(id) {
   emailEditar.value = usuarioEncontrado.correo;
   passEditar.value = usuarioEncontrado.pass;
   usuarioId = usuarioEncontrado.id;
-  
 }
 
 function editarUsuario(e) {
   e.preventDefault();
-  const usuarioModificado = usuarios.map((usuario) => (usuario.id === usuarioId) ? {
-      ...usuario,
-      nombre: nombreEditar.value,
-      apellido: apellidoEditar.value,
-      email: emailEditar.value,
-      pass: passEditar.value,
-  }
+  const usuarioModificado = usuarios.map((usuario) =>
+    usuario.id === usuarioId
+      ? {
+          ...usuario,
+          nombre: nombreEditar.value,
+          apellido: apellidoEditar.value,
+          email: emailEditar.value,
+          pass: passEditar.value,
+        }
       : usuario
   );
 
   const json2 = JSON.stringify(usuarioModificado);
-  localStorage.setItem('usuarios', json2);
+  localStorage.setItem("usuarios", json2);
   usuarios = usuarioModificado;
   mostrarUsuarios();
-  
-};
+}
 
 mostrarUsuarios();
 editarFormUsuarios.onsubmit = editarUsuario;
